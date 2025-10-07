207cdb56ad59b26216a5c926c64914deec05554a
Cleverton Neves <clever.nvs@gmail.com>
2025-10-07 16:42:17 -0300

    fix: Removendo arquivos de refatoração

--- api/src/config/cors.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-    res.header('Acess-Control-Allow-Orign', '*')
-    res.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    next()
-}
